6d0b29f6136e8d9cb15c1a74deebe251038b4a15
av-gowtham <gowthamavg10@gmail.com>
2024-07-11 17:50:27 +0530

    Added the react-router-dom package

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Weather } from "./Components/Weather.jsx";
+import { HashRouter } from "react-router-dom";
 import "./Weather.css";
 
 function App() {
   return (
-    <>
+    <HashRouter>
       <div className="app">
         <Weather />
       </div>
-    </>
+    </HashRouter>
   );
 }
 
